a5824bc248f51f99b2ea206e0b682ac6f42ff232
WH1TE-SHAD0W <marek@culak.sk>
2021-10-19 09:52:49 +0200

    nejde to tu

--- zenit 3.py
+++ zenit 3.py
@@ -1,6 +1,20 @@
 t= int(input())
 a =[]
 for i in range(t):
-    a.append(input())
-print(a)
+    a.append(int(input()))
 
+def Digits(n):
+
+    minimum = 9
+
+    while (n):
+        r = n % 10
+
+        minimum = min(r, minimum)
+
+        n = n // 10
+
+    print(minimum)
+
+for i in range(t):
+    Digits(a[i])
