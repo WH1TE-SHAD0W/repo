c30601676ca253d69eccb1e9130f0394025e6a78
WH1TE-SHAD0W <marek@culak.sk>
2021-10-19 11:15:58 +0200

    lepsie

--- zenit 4.py
+++ zenit 4.py
@@ -7,10 +7,12 @@ for i in range(t):
     a.append(int(input()))
 
 for i in range(t-1):
-    if i <= t:
-        if a[i] > a[i-1]:
-            horsie += 1
-        elif a[i] > a[i+1]:
+    if a[i] > a[i-1]:
+        horsie += 1
+        print(',')
+    elif a[i] < a[i-1]:
+        if i!=0:
+            print('.')
             lepsie += 1
     if a[i] > a[i+1] and a[i] > a[i-1]:
         vlna += 1
