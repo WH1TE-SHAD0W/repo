56a87509666fe24b29603d0e2bad607d28b20693
WH1TE-SHAD0W <marek@culak.sk>
2022-05-18 14:10:32 +0200

    delitelnost

--- zenit 6.py
+++ zenit 6.py
@@ -3,11 +3,8 @@ a = input().split()
 a[2]=float(a[2])
 a[0]=int(a[0])
 a[1]=int(a[1])
-v=0
 p=a[0]
 for i in range(a[1]):
-    if a[2] > 0.5:
-        v+=p*2*a[2]
-    else:
-        v=p
-print(int(v))
+    if a[2] >= 0.5:
+        p=p*2*a[2]
+print(p)
